5d9375066641026d91023b8e691b01ad8b366419
nauvalalpen <nauvalalpenperdana@gmail.com>
2025-04-21 21:16:24 +0700

    add new api - weather

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\PremierLeagueTopAssistController;
 use App\Http\Controllers\TopAssistController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeatherController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,6 @@ Route::get('/', function () {
 // Route untuk mendapatkan Top Assist dari Premier League
 Route::get('api/top-assist', [PremierLeagueTopAssistController::class,'getTopAssist']);
 Route::get('api/top-assist', [PremierLeagueTopAssistController::class, 'index']);
+
+Route::get('/weather', [WeatherController::class, 'index'])->name('weather.index');
+Route::get('/weather/update', [WeatherController::class, 'updateWeatherInfo'])->name('weather.update');
